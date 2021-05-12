d8529761b360104161c806302ce6eb0e04f0f74f
Olha-Starykova <olenkalog@gmail.com>
2021-05-12 22:24:05 +0300

    com

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,3 +1,99 @@
+// import React, { useState} from 'react';
+// import Card from '@material-ui/core/Card';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import Button from '@material-ui/core/Button';
+//  import { connect } from 'react-redux';
+//  import { authOperations } from '../redux/auth';
+
+
+// const styles = {
+//   form: {
+//     width: 320,
+//   },
+//   label: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     margin: 20,
+//   },
+//   card: {
+//     margin: 15,
+//   },
+//   logoDiv: {
+//     padding: 20,
+//    }
+// };
+
+// const LoginView = () => { 
+
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const updateEmail = evt => {
+//     setEmail(evt.target.value);
+//   };
+
+//   const updatePassword = evt => {
+//     setPassword(evt.target.value)
+//   };
+// //============================================================================================?????????????
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onLogin(this.state);
+
+//     this.setState({ name: '', email: '', password: '' });
+//   };
+
+
+//     return (
+//       <div style={styles.logoDiv}>
+//         <Card style={styles.card}>
+//           <CardActionArea>
+//             <h1>Страница логина</h1>
+
+//             <form
+//               onSubmit={handleSubmit}
+//               style={styles.form}
+//               autoComplete="off"
+//             >
+//               <label style={styles.label}>
+//                 Почта
+//             <input
+//                   type="email"
+//                   name="email"
+//                   value={email}
+//                   onChange={updateEmail}
+//                 />
+//               </label>
+
+//               <label style={styles.label}>
+//                 Пароль
+//             <input
+//                   type="password"
+//                   name="password"
+//                   value={password}
+//                   onChange={updatePassword}
+//                 />
+//               </label>
+//               <Button type="submit" size="small" color="secondary">
+//                 Войти
+//               </Button>
+//               {/* <button type="submit">Войти</button> */}
+//             </form>
+//           </CardActionArea>
+//         </Card>
+//       </div>
+//     );
+//   }
+
+
+// const mapDispatchToProps = {
+//   onLogin: authOperations.logIn,
+// };
+
+// export default connect(null, mapDispatchToProps)(LoginView);
+
+
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
