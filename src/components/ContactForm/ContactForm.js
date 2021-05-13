01e4bfc145a1eab6fc1f4f1453c2126b3c6506e3
Olha-Starykova <olenkalog@gmail.com>
2021-05-14 01:21:06 +0300

    com 35

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,74 +1,106 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from "prop-types";
   //ставим npm i shortid
 import shortid from 'shortid';
 import './ContactForm.css'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import contactsOperations from '../../redux/contacts/contacts-operations'
 import contactsSelectors from '../../redux/contacts/contacts-selectors'
 
   
 
-class ContactForm extends Component {
-  state = {
-    name: '', 
-    number: ''
-  };
-//генерим там лежит уникалый индефикатор
-  nameInputId = shortid.generate();
-  numberInputId = shortid.generate();
+export default function ContactForm() {
+  const dispatch = useDispatch()
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+    const contacts = useSelector(contactsSelectors.getAllTodos)
+
+
+//   state = {
+//     name: '', 
+//     number: ''
+//   };
+// //генерим там лежит уникалый индефикатор
+  let nameInputId = shortid.generate()
+  let numberInputId = shortid.generate();
+
+  const hendelChangeName = useCallback( e => {
+    setName(e.currentTarget.value)
+  },[]);
+  const hendelChangeNumber = useCallback(e => {
+    setNumber(e.currentTarget.value)
+  },[]);
    
-    //общий метод для инпутов патерн для форм по внешнему виду контролируемый елемент
-    hendelChange = e => {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value,
-        })
-    };
-    //сабмит формы
-    hendelSubmit = e => { 
-        e.preventDefault();
-        if (this.props.contacts.map(({ name }) => name).includes(this.state.name)) {
-        alert(`${this.state.name} is already in contacts`)
+  const   hendelSubmit = useCallback( e => { 
+    e.preventDefault();
+    
+        if (contacts.map(({ name }) => name).includes(name)) {
+        alert(`${name} is already in contacts`)
         return
-      }
-       this.props.onSubmit( this.state.name, this.state.number)
+    }
+     dispatch(contactsOperations.addTodo(name, number)) 
+//this.props.onSubmit( this.state.name, this.state.number)
       
-        this.reset();
-    };
+
+   
     //сброс формы очистить стейт
-  reset = () => {
-    this.setState({
-      name: '',
-      number: ''
-    })
-  };
+ 
+    setName('')
+    setNumber('')
+ },[dispatch, name, number])
+
+//     //общий метод для инпутов патерн для форм по внешнему виду контролируемый елемент
+//     hendelChange = e => {
+//         const { name, value } = e.target;
+//         this.setState({
+//             [name]: value,
+//         })
+//     };
+//     //сабмит формы
+//     hendelSubmit = e => { 
+//         e.preventDefault();
+//         if (this.props.contacts.map(({ name }) => name).includes(this.state.name)) {
+//         alert(`${this.state.name} is already in contacts`)
+//         return
+//       }
+//        this.props.onSubmit( this.state.name, this.state.number)
+      
+//         this.reset();
+//     };
+//     //сброс формы очистить стейт
+//   reset = () => {
+//     this.setState({
+//       name: '',
+//       number: ''
+//     })
+//   };
 
 
-    render() {
+    
       return (
-        <form className='form' onSubmit={this.hendelSubmit}>
-          <label htmlFor={this.nameInputId}>
+        <form className='form' onSubmit={hendelSubmit}>
+          <label htmlFor={nameInputId}>
             Name <input
               type="text"
-              value={this.state.name}
+              value={name}
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
               required
-              id={this.nameInputId}
-              onChange={this.hendelChange}
+              id={nameInputId}
+              onChange={hendelChangeName}
             />
           </label>
-          <label htmlFor={this.numberInputId}>
+          <label htmlFor={numberInputId}>
             Number <input
               type="tel"
-              value={this.state.number}
+              value={number}
               name="number"
               //pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
               title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
-              id={this.numberInputId}
-              onChange={this.hendelChange}
+              id={numberInputId}
+              onChange={hendelChangeNumber}
               required
             />
           </label>
@@ -79,7 +111,6 @@ class ContactForm extends Component {
             
       );
   };
-};
 
 
    ContactForm.propTypes = {
@@ -87,15 +118,15 @@ class ContactForm extends Component {
 };
 
 
-const mapStateToProps = state => {
-//  return {contacts: state.contacts.contacts}
-   return {contacts: contactsSelectors.getAllTodos(state) }
-  }
+// const mapStateToProps = state => {
+// //  return {contacts: state.contacts.contacts}
+//    return {contacts: contactsSelectors.getAllTodos(state) }
+//   }
 
 
-const mapDispatchToProps = dispatch => ({
-    onSubmit: (name, number) => dispatch(contactsOperations.addTodo(name, number)) 
-})
+// const mapDispatchToProps = dispatch => ({
+//     onSubmit: (name, number) => dispatch(contactsOperations.addTodo(name, number)) 
+// })
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
+// export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
